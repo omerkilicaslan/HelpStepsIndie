5d5059d299ecc669e6d5a2df706e9d4e9eb8fc64
omerkilicaslan <omerrr_2001@hotmail.com>
2023-09-14 20:24:06 +0300

    Initial Commit

---
+++ HelpStepsClone/HelpStepsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelpStepsCloneApp.swift
+//  HelpStepsClone
+//
+//  Created by Ömer Faruk Kılıçaslan on 14.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HelpStepsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
