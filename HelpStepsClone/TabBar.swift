2db1684ee5a3f973ee2d10adbe066dac31fdbe43
Mert Kanak <107624227+mertkanak@users.noreply.github.com>
2023-09-14 21:31:37 +0300

    tabbarCustomized

--- HelpStepsClone/TabBar.swift
+++ HelpStepsClone/TabBar.swift
@@ -12,32 +12,32 @@ struct TabBar: View {
         TabView {
             Text("Ana Sayfa")
                 .tabItem {
-                    Image(systemName: "1.circle")
+                    Image(systemName: "house")
                     Text("Ana Sayfa")
                 }
                 .tag(0)
 
             Text("Aktiviteler")
                 .tabItem {
-                    Image(systemName: "2.circle")
+                    Image(systemName: "figure.run.square.stack.fill")
                     Text("Aktiviteler")
                 }
                 .tag(1)
             Text("NE SANDIN KANKS")
                 .tabItem {
-                    Image(systemName: "3.circle")
+                    Image(systemName: "shadow")
                     Text("")
                 .tag(2)
                 }
             Text("Topluluk")
                 .tabItem {
-                    Image(systemName: "4.circle")
+                    Image(systemName: "person.3")
                     Text("Topluluk")
                 .tag(3)
                 }
             Text("Profil")
                 .tabItem {
-                    Image(systemName: "5.circle")
+                    Image(systemName: "person")
                     Text("Profil")
                 .tag(4)
             }
